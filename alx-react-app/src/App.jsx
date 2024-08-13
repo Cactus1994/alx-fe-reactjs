78d00e08e5d9fd7732dfae4b8c7e19e836bc9218
cactus1994 <souhailsabbar48@gmail.com>
2024-08-13 19:38:20 +0100

    task3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -16,6 +17,7 @@ function App() {
       <Header />
       <MainContent />
       <WelcomeMessage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
       <div>
         <a href="https://vitejs.dev" target="_blank">
